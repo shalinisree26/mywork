496e89c34cf5ffcb1bb7f85c003eaadfcdefbcb0
shalinisree26 <126102119+shalinisree26@users.noreply.github.com>
2023-02-23 00:20:13 +0530

    Add files via upload

---
+++ day1/mobile.java
@@ -0,0 +1,24 @@
+package week1.day1;
+
+public class mobile {
+	
+	public void makeCall() {
+		// TODO Auto-generated method stub
+		System.out.println("make the call plz");
+
+	}
+	public void sendMsg() {
+		// TODO Auto-generated method stub
+System.out.println("send themessage");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		mobile good=new mobile();
+good.makeCall();
+good.sendMsg();
+				
+
+	}
+
+}
